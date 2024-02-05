158449a68db291cb320d4c033c9a915a81414410
Roberts Vesīks <alex36254@gmail.com>
2024-02-05 23:48:29 +0300

    WebSocket server init

---
+++ WalkieTalkieSockets/WalkieTalkieSockets/src/main/java/me/pe4en1e/WalkieTalkieSockets/WalkieTalkieSocketsApplication.java
@@ -0,0 +1,13 @@
+package me.pe4en1e.WalkieTalkieSockets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalkieTalkieSocketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalkieTalkieSocketsApplication.class, args);
+	}
+
+}
