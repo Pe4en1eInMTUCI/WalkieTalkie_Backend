307b4cdd1e6829c316ec91740e9e65158103284e
Roberts Vesīks <alex36254@gmail.com>
2024-02-09 01:02:36 +0300

    swagger added

--- WalkieTalkieAuthService/src/main/java/me/pe4en1e/WalkieTalkieAuthService/RequestController.java
+++ WalkieTalkieAuthService/src/main/java/me/pe4en1e/WalkieTalkieAuthService/RequestController.java
@@ -1,20 +1,30 @@
 package me.pe4en1e.WalkieTalkieAuthService;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
+
+@Tag(name = "Test RequestController", description = "Тестовый Request контроллер")
 public class RequestController {
 
-    @RequestMapping("/")
-    public ResponseEntity test() {
+    @GetMapping("/test")
+    @Operation(summary = "Тестовый метод", description = "Возвращает ту же строчку")
+    public String test(String string) {
         try {
-            return ResponseEntity.ok("OK!");
+            return "You wrote " + string;
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body("Error!");
+            System.out.println(e.getMessage());
+            return "Error!";
+
         }
     }
+
 
 }
--- WalkieTalkieAuthService/src/main/java/me/pe4en1e/WalkieTalkieAuthService/WalkieTalkieAuthServiceApplication.java
+++ WalkieTalkieAuthService/src/main/java/me/pe4en1e/WalkieTalkieAuthService/WalkieTalkieAuthServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WalkieTalkieAuthServiceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		SpringApplication.run(WalkieTalkieAuthServiceApplication.class, args);
 	}
 
