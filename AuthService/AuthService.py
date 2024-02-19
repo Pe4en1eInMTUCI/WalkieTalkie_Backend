db4ab56bce4cb02394cbe5f2358d6fa744356bdc
Roberts Vesīks <alex36254@gmail.com>
2024-02-19 04:34:59 +0300

    Setting up database

--- AuthService/AuthService.py
+++ AuthService/AuthService.py
@@ -1,12 +1,39 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 import requests
 import json
 
+import DataBaseWorker as db
 
+
+app = FastAPI()
+
+
+@app.get("/api")
+def default():
+    return "WalkieTalkie AuthService is working!"
+
+
+@app.get("/api/sendcode")
 def sendCode(userIP, userPhone):
     response = requests.get(f"https://sms.ru/code/call?phone={userPhone}&ip={userIP}&api_id=2356D300-3BA5-FCFD-7CE5-4F800426C6BD").json()
-    print(json.dumps(response))
+    return response["code"]
 
 
-sendCode("-1", "79778614427")
+@app.post("/api/register")
+def regUser(phone: int = Form(), username: str = Form(...), password: str = Form(...)):
+    if not db.userExists(username):
+        return db.addUser(phone, username, password)
+    else:
+        return 'Error: User exists!'
+
+
+
+
+@app.post("/api/login")
+def loginUser():
+    pass
+
+
+
+
--- AuthService/DataBaseWorker.py
+++ AuthService/DataBaseWorker.py
@@ -1,2 +1,38 @@
 
+import sqlite3
+from hashlib import sha256
+import secrets
 
+
+def userExists(username):
+    try:
+        connection = sqlite3.connect("./WalkieTalkie.db")
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+        result = cursor.fetchall()
+
+        connection.close()
+
+        return bool(len(result))
+    except:
+        return False
+
+
+def addUser(phone, username, password):
+    try:
+        connection = sqlite3.connect("./WalkieTalkie.db")
+        cursor = connection.cursor()
+
+        password = sha256(password.encode("UTF-8".hexdigest)).hexdigest()
+
+        cursor.execute("INSERT INTO users VALUES phone, username, password (?, ?, ?)", (phone, username, password,))
+
+        connection.close()
+        return "User added!"
+    except:
+        return "Error!"
+
+
+
+addUser("79778614427", "pe4en1e", "somepassword")
