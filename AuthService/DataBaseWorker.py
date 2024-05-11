ba34a4e1fea8ee9bc0b4a9e82b7f3b1397b3e73c
Roberts Vesīks <alex36254@gmail.com>
2024-05-11 07:35:45 +0300

    useridbyphone, userchats methods

--- AuthService/DataBaseWorker.py
+++ AuthService/DataBaseWorker.py
@@ -116,3 +116,15 @@ def getDialogs(username):
 
 
 
+def getUsernameByPhone(phone):
+    connection = sqlite3.connect("WalkieTalkie.db")
+    cursor = connection.cursor()
+
+    result = cursor.execute("SELECT username FROM users WHERE phone = ?", (phone,)).fetchall()[0][0]
+
+    connection.commit()
+    connection.close()
+
+    return result
+
+
